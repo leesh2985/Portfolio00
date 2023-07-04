7d7745d7b6d85b7b2e00b3456294e4ef5e4dff53
Seung Lee <leesh2985@naver.com>
2023-07-04 17:22:38 +0900

    모드변경...시도..

---
+++ src/theme/theme.tsx
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  bgColor: '#fff',
+  textColor: '#242424 ',
+};
+
+export const darkTheme = {
+  bgColor: '#282c35',
+  textColor: '#fff',
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
