9d0256ce47b24c08fbc0ad83c3528dcf7b70679c
Seung Lee <leesh2985@naver.com>
2023-07-06 01:11:19 +0900

    모드 넣어줌

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
 import Carousel from './component/carousel/Carousel';
-import Header from './component/Header';
+import Header from './component/header/Header';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './style/GlobalStyle';
+import { darkTheme, lightTheme } from './style/theme';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+
   return (
-    <>
-      <Header />
-      <Carousel />
-    </>
+    <React.Fragment>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <GlobalStyle theme={theme === 'light' ? lightTheme : darkTheme} />
+        <Header toggleTheme={toggleTheme} theme={theme} />
+        <Carousel />
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
