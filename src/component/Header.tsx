ca508c5a09c4b6f198bd0ff7139bb126a9608cfc
Seung Lee <leesh2985@naver.com>
2023-07-05 15:43:07 +0900

    중앙정렬

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -37,7 +37,8 @@ const Gnb = styled.div`
 
   align-items: center;
   justify-content: space-between;
-  /* background-color: red; */
+  max-width: 1280px;
+  margin: 0 auto; /* background-color: red; */
   height: 60px;
 `;
 
