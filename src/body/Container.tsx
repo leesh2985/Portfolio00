57223210a2bc18b0d2e452a829af4f6d184311e1
Seung Lee <leesh2985@naver.com>
2023-07-07 03:24:16 +0900

    넣을꺼넣음

--- src/body/Container.tsx
+++ src/body/Container.tsx
@@ -1,14 +1,12 @@
 import { styled } from 'styled-components';
 import Left from './Left';
 import Right from './Right';
-import Middle from './Middle';
 
 export default function Container() {
   return (
     <Body>
       <Left />
       <Right />
-      <Middle />
     </Body>
   );
 }
@@ -16,7 +14,7 @@ export default function Container() {
 const Body = styled.div`
   position: relative;
   max-width: 1280px;
-  height: 500px;
+  height: 700px;
   margin: 0 auto;
   padding-top: 50px;
   padding-bottom: 60px;
