d51ec872f20319ef5daa52421770e8d829d8864e
Seung Lee <leesh2985@naver.com>
2023-07-07 03:24:36 +0900

    모드 변경이상함 틀잡은 뒤 다시 수정

--- src/body/Right.tsx
+++ src/body/Right.tsx
@@ -1,27 +1,36 @@
-import styled from 'styled-components';
+import styled, { StyleSheetManager } from 'styled-components';
+
+interface RightSectionProps {
+  darkMode: boolean;
+}
 
 export default function Right() {
+  const isDarkMode = true;
+
   return (
-    <RightSection>
-      <LoginLink>로그인</LoginLink>
-      <SocialLink>Goolgle로 시작하기</SocialLink>
-      <LoginInfo>
-        <IdLink>아이디 찾기</IdLink>
-        <PwsLink>비밀번호 찾기</PwsLink>
-        <JoinLink>회원가입하기</JoinLink>
-      </LoginInfo>
-    </RightSection>
+    <StyleSheetManager shouldForwardProp={prop => prop !== 'darkMode'}>
+      <RightSection darkMode={isDarkMode}>
+        <LoginLink>로그인</LoginLink>
+        <SocialLink>Goolgle로 시작하기</SocialLink>
+        <LoginInfo>
+          <IdLink>아이디 찾기</IdLink>
+          <PwsLink>비밀번호 찾기</PwsLink>
+          <JoinLink>회원가입하기</JoinLink>
+        </LoginInfo>
+      </RightSection>
+    </StyleSheetManager>
   );
 }
 
-const RightSection = styled.section`
+const RightSection = styled.section<RightSectionProps>`
   float: right;
-  width: 520px;
+  width: 435px;
   border: 1px solid #d3d3d3;
   border-radius: 10px;
   padding: 21px 20px 18px;
   line-height: 20px;
   text-align: center;
+  background-color: #fff;
 `;
 
 const LoginLink = styled.a`
@@ -33,7 +42,7 @@ const LoginLink = styled.a`
   border: 1px solid;
   display: block;
   color: #fff;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: bold;
   border-radius: 4px;
   background-color: #41b6e6;
@@ -52,7 +61,7 @@ const SocialLink = styled.a`
   border: 1px solid;
   display: block;
   color: #fff;
-  font-size: 20px;
+  font-size: 18px;
   border-radius: 4px;
   background-color: #a9a9a9;
 `;
