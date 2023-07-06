fac58aaa7f80dd70111c6bf5885b46b16d52deff
Seung Lee <leesh2985@naver.com>
2023-07-07 03:21:48 +0900

    바디 넣을 꺼 넣음

--- src/body/Container.tsx
+++ src/body/Container.tsx
@@ -1,12 +1,14 @@
 import { styled } from 'styled-components';
 import Left from './Left';
 import Right from './Right';
+import Middle from './Middle';
 
 export default function Container() {
   return (
     <Body>
       <Left />
       <Right />
+      <Middle />
     </Body>
   );
 }
