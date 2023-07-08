ac91c3474cb48d6c6e1784952b9799ce55eb9a6c
Seung Lee <leesh2985@naver.com>
2023-07-09 03:20:01 +0900

    구역을 다 나눠주는게 좋을뜻하다.

--- src/body/Container.tsx
+++ src/body/Container.tsx
@@ -1,12 +1,16 @@
 import { styled } from 'styled-components';
 import Left from './left/Left';
 import Right from './right/Right';
+import Record from './left/Record';
+import Tabs from './right/Tabs';
 
 export default function Container() {
   return (
     <Body>
       <Left />
+      <Record />
       <Right />
+      <Tabs />
     </Body>
   );
 }
