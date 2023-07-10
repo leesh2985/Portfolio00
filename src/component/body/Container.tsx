d888ca10bc67729ff38c4acb6e35fbce67132d41
Seung Lee <leesh2985@naver.com>
2023-07-10 14:26:07 +0900

    위치 오류 및 마진 플렉스박스로 함

--- src/component/body/Container.tsx
+++ src/component/body/Container.tsx
@@ -8,18 +8,21 @@ export default function Container() {
   return (
     <Body>
       <Left />
-      <Record />
       <Right />
+      <Record />
       <Tabs />
     </Body>
   );
 }
 
 const Body = styled.div`
+  display: flex;
   position: relative;
   max-width: 1280px;
   height: 700px;
   margin: 0 auto;
   padding-top: 50px;
   padding-bottom: 60px;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
