c162d68ddb31614e60685a51dacf702ac206b4a1
Seung Lee <leesh2985@naver.com>
2023-07-13 02:26:47 +0900

    로그인창

--- src/component/body/right/loginfolder/JoinLonin.tsx
+++ src/component/body/right/loginfolder/JoinLonin.tsx
@@ -1,5 +1,123 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 export default function JoinLonin() {
-  return <div>JoinLonin</div>;
+  return (
+    <>
+      <LogoArea>
+        <Logo to="/home">
+          <LogoImg src="/img/logo.png" alt="로고" />
+        </Logo>
+        <LogoText>회원가입</LogoText>
+      </LogoArea>
+
+      <Form>
+        <NameInput type="text" name="text" placeholder="이름(실명)" />
+        <HbdInput type="date" name="date" placeholder="생년월일 8자리" />
+        <IdInput type="email" name="email" placeholder="아이디" />
+        <PWInput type="password" name="password" placeholder="비밀번호" />
+        <PWInputCheck type="password" name="password" placeholder="비밀번호확인" />
+      </Form>
+      <Join>
+        <JoinHdrc>가입하기</JoinHdrc>
+      </Join>
+    </>
+  );
 }
+
+const LogoArea = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 700px;
+  margin: 0 auto;
+  height: 60px;
+`;
+
+const Logo = styled(Link)`
+  cursor: pointer;
+  width: 200px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const LogoImg = styled.img`
+  width: 100px;
+  padding: 25px 115px 25px 0px;
+`;
+
+const buttonStyles = css`
+  cursor: pointer;
+  width: 100%;
+  height: 50px;
+  padding: 10px 0;
+  border: 1px solid;
+  display: block;
+  color: #fff;
+  font-size: 18px;
+  border-radius: 4px;
+`;
+
+const Form = styled.form`
+  max-width: 700px;
+  margin: 0 auto;
+`;
+
+const inputStyles = css`
+  width: 100%;
+  height: 30px;
+  padding: 10px 0;
+  border: 1px solid;
+  display: block;
+  color: #000000;
+  font-size: 18px;
+  border-radius: 4px;
+  margin-top: 5px;
+  background-color: #fff;
+  border: 1px solid #808080;
+
+  &::placeholder {
+    padding-left: 15px;
+  }
+`;
+
+const NameInput = styled.input`
+  ${inputStyles}
+  margin-top: 50px;
+`;
+
+const HbdInput = styled.input`
+  ${inputStyles}
+`;
+
+const IdInput = styled.input`
+  ${inputStyles}
+  margin-top: 70px;
+
+  &::placeholder {
+    content: '@';
+  }
+`;
+
+const PWInput = styled.input`
+  ${inputStyles}
+`;
+
+const PWInputCheck = styled.input`
+  ${inputStyles}
+`;
+
+const Join = styled.div`
+  margin: 0 auto;
+  max-width: 700px;
+`;
+
+const JoinHdrc = styled.button`
+  ${buttonStyles}
+  margin-top: 100px;
+  background-color: #1e8ec7;
+  border-color: #1e8ec7;
+`;
