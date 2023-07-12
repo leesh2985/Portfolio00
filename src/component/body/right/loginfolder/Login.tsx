fd528ddbdba9ff2a8a133b270f31ae5649d4217c
Seung Lee <leesh2985@naver.com>
2023-07-12 22:25:00 +0900

    다른 방법으로 수정

--- src/component/body/right/loginfolder/Login.tsx
+++ src/component/body/right/loginfolder/Login.tsx
@@ -1,22 +1,50 @@
+import { useEffect, useState } from 'react';
+import { auth } from './FireBase';
+import { GoogleAuthProvider, User, signInWithPopup } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import styled, { StyleSheetManager } from 'styled-components';
+// import Home from '../../../Home';
 
 interface LoginSectionProps {
   darkMode: boolean;
 }
 
 export default function Login() {
+  const [value, setValue] = useState<User | null>(null);
+  const handleClick = () => {
+    const provider = new GoogleAuthProvider(); // provider를 구글로 설정
+    signInWithPopup(auth, provider) // popup을 이용한 signup
+      .then(data => {
+        setValue(data.user); // user data 설정
+        console.log(data); // console로 들어온 데이터 표시
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   const isDarkMode = true;
+
+  // useEffect(() => {
+  //   setValue(JSON.parse(localStorage.getItem('email') || 'null'));
+  // }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload;
+  };
 
   return (
     <StyleSheetManager shouldForwardProp={prop => prop !== 'darkMode'}>
       <LoginSection darkMode={isDarkMode}>
         <LoginLink>로그인</LoginLink>
-        <SocialLink to="/google-signin">Goolgle로 시작하기</SocialLink>
+        <SocialLink onClick={handleClick}>Goolgle로 시작하기</SocialLink>
+        {value ? value.displayName : null}
         <LoginInfo>
           <IdLink>아이디 찾기</IdLink>
           <PwsLink>비밀번호 찾기</PwsLink>
           <JoinLink to="/join-login">회원가입하기</JoinLink>
+          <Logout onClick={logout}>Logout</Logout>
         </LoginInfo>
       </LoginSection>
     </StyleSheetManager>
@@ -53,7 +81,7 @@ const LoginLink = styled.a`
   }
 `;
 
-const SocialLink = styled(Link)`
+const SocialLink = styled.button`
   cursor: pointer;
   margin: 0 auto;
   width: 80%;
@@ -62,7 +90,6 @@ const SocialLink = styled(Link)`
   border: 1px solid;
   display: block;
   color: #fff;
-  text-decoration: none;
   font-size: 18px;
   border-radius: 4px;
   background-color: #a9a9a9;
@@ -78,6 +105,7 @@ const IdLink = styled.a`
   cursor: pointer;
   font-size: 13px;
 `;
+
 const PwsLink = styled.a`
   cursor: pointer;
   font-size: 13px;
@@ -97,6 +125,20 @@ const JoinLink = styled(Link)`
   font-size: 13px;
   color: #242424;
   text-decoration: none;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 1px;
+    height: 12px;
+    background-color: #a9a9a9;
+    margin: 4px 12px 0;
+  }
+`;
+
+const Logout = styled.button`
+  cursor: pointer;
+  font-size: 13px;
 
   &::before {
     content: '';
