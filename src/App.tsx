e3a9b76d17625083c55099f73f6c83648439f896
Seung Lee <leesh2985@naver.com>
2023-07-13 01:07:05 +0900

    회원가입및 로그인 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import Home from './component/Home';
 import JoinLonin from './component/body/right/loginfolder/JoinLonin';
+import MyLonin from './component/body/right/loginfolder/MyLonin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/my-lonin" element={<MyLonin />} />
         <Route path="/join-login" element={<JoinLonin />} />
       </Routes>
     </BrowserRouter>
