901e8abb70dafd373b3eaa60f3c87aed8e227241
Seung Lee <leesh2985@naver.com>
2023-07-12 19:49:47 +0900

    라우터 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './component/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
