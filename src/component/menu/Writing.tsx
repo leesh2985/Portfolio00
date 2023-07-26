bfed2548533e3ea265f30b9b876b581450cc2f96
Seung Lee <leesh2985@naver.com>
2023-07-26 23:03:33 +0900

    글쓰기 제작

--- src/component/menu/Writing.tsx
+++ src/component/menu/Writing.tsx
@@ -1,6 +1,31 @@
-import { Editor } from '@toast-ui/react-editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
+import { useMemo, useRef, useState } from 'react';
+import JoditEditor from 'jodit-react';
+import { styled } from 'styled-components';
 
 export default function Writing() {
-  return <div>Writing</div>;
+  const editor = useRef(null);
+  const [content, setContent] = useState('');
+
+  return (
+    <WritingContainer>
+      <JoditEditor ref={editor} value={content} onChange={newContent => setContent(newContent)} />
+
+      <CheckBtn>확인</CheckBtn>
+    </WritingContainer>
+  );
 }
+
+const WritingContainer = styled.div`
+  max-width: 1380px;
+  margin: 0 auto;
+  margin-top: 50px;
+`;
+const CheckBtn = styled.button`
+  background-color: #1e8ec7;
+  border: 1px solid #1e8ec7;
+  font-size: 15px;
+  border-radius: 20px;
+  padding: 10px 15px;
+  color: #fff;
+  margin: 20px 0;
+`;
