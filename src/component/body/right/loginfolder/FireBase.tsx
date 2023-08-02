5375b05365b42cfca37395acdd8e3dea1cc39156
Seung Lee <leesh2985@naver.com>
2023-08-02 20:53:09 +0900

    const dbService = firebase.firestore(); 추가

--- src/component/body/right/loginfolder/FireBase.tsx
+++ src/component/body/right/loginfolder/FireBase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import firebase from 'firebase/compat/app';
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const dbService = firebase.firestore();
 
-export { app, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export { app, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, dbService };
--- src/component/menu/Writing.tsx
+++ src/component/menu/Writing.tsx
@@ -2,20 +2,23 @@ import { useEffect, useRef, useState } from 'react';
 import JoditEditor, { Jodit } from 'jodit-react'; // Import Jodit type
 import { styled } from 'styled-components';
 import { User } from 'firebase/auth';
-import { auth } from '../body/right/loginfolder/FireBase';
+import { auth, dbService } from '../body/right/loginfolder/FireBase';
 import LoginPage from '../body/right/loginfolder/LoginPage';
+import { collection, addDoc } from 'firebase/firestore';
 
-export default function Writing() {
+export default async function Writing() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const editor = useRef<Jodit>(null); // Specify the type as Jodit
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
+  const [userObj, setUserObj] = useState<User | null>(null); // User 타입의 상태를 추가합니다.
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user: User | null) => {
       // User 타입을 명시적으로 지정합니다.
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user); // 사용자 정보를 저장합니다.
       } else {
         setIsLoggedIn(false);
       }
@@ -29,11 +32,13 @@ export default function Writing() {
     return <LoginPage />;
   }
 
-  const handleCheckBtnClick = () => {
-    const currentContent = editor.current?.value; // 현재 에디터의 내용 가져오기
-    console.log('제목:', title);
-    console.log('추가된 내용:', currentContent);
-    // 이후 추가된 내용을 서버에 저장하거나 다른 처리를 수행할 수 있습니다.
+  const handleCheckBtnClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    await dbService.collection('messages').add({
+      id: 1,
+      title: title,
+      userId: userObj?.displayName,
+    });
   };
 
   return (
@@ -70,7 +75,7 @@ const InputDiv = styled.div`
 `;
 
 const SelectInput = styled.select`
-  font-size: 20px;
+  font-size: 18px;
   padding: 10px;
   margin-right: 10px;
   margin-bottom: 10px;
