4f8b5540c459f3da6722c5dbfd3a0443f0943b4d
Seung Lee <leesh2985@naver.com>
2023-08-07 20:44:28 +0900

    기본틀

--- src/component/menu/PostDetail.tsx
+++ src/component/menu/PostDetail.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 export default function PostDetail() {
@@ -13,7 +14,22 @@ export default function PostDetail() {
           <PostItem>userId: doc.data().userId,</PostItem>
           <PostItem>날짜</PostItem>
         </PostInfo>
+        <PostContents>
+          <PostText></PostText>
+          <PostLike>❤️ 0</PostLike>
+        </PostContents>
+        <ReplyContents>
+          <ReplyTitle>전체댓글 0개</ReplyTitle>
+          <ReplyInfo>
+            <ReplyItem>글쓴이</ReplyItem>
+            <ReplyItem>댓글</ReplyItem>
+            <ReplyItem>날짜</ReplyItem>
+          </ReplyInfo>
+          <ReplyInput>로그인 후 댓글을 작성할수 있습니다.</ReplyInput>
+        </ReplyContents>
       </PostContainer>
+      <WriteBtn to="/writing">글쓰기</WriteBtn>
+      <ListBtn to="/contest">목록</ListBtn>
     </Container>
   );
 }
@@ -34,10 +50,14 @@ const Title = styled.p`
 `;
 
 const PostContainer = styled.div``;
+
 const PostIitle = styled.h2``;
+
 const PostInfo = styled.ul`
   display: flex;
   flex-flow: row nowrap;
+  border-top: 3px solid #0077b3;
+  border-bottom: 2px solid #0077b3;
 `;
 const PostItem = styled.li`
   padding: 8px 6px;
@@ -51,4 +71,51 @@ const PostItem = styled.li`
   &:nth-child(n + 4) {
     order: -1;
   }
+`;
+
+const PostContents = styled.div`
+  border-bottom: 3px solid #0077b3;
+`;
+
+const PostText = styled.p``;
+const PostLike = styled.span``;
+
+const ReplyContents = styled.div``;
+const ReplyTitle = styled.div``;
+
+const ReplyInfo = styled.ul`
+  display: flex;
+  flex-flow: row nowrap;
+  border-top: 3px solid #0077b3;
+`;
+
+const ReplyItem = styled.li`
+  border-bottom: 1px solid #0077b3;
+`;
+
+const ReplyInput = styled.div``;
+
+const WriteBtn = styled(Link)`
+  background-color: #1e8ec7;
+  border: 1px solid #1e8ec7;
+  font-size: 15px;
+  border-radius: 15px;
+  color: #fff;
+  height: auto;
+  padding: 10px 15px;
+  position: absolute;
+  right: 0;
+  text-decoration: none;
+`;
+
+const ListBtn = styled(Link)`
+  background-color: #fff;
+  border: 1px solid #242424;
+  font-size: 15px;
+  border-radius: 15px;
+  height: auto;
+  padding: 10px 15px;
+  position: absolute;
+  right: 0;
+  text-decoration: none;
 `;
