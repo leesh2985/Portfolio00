bc25c5e83e84d9de95c1dcc9c6f3fe23ed8e77e0
Seung Lee <leesh2985@naver.com>
2023-08-09 02:27:47 +0900

    로그인 유무 댓글기능활성화

--- src/component/menu/ReplyForm.tsx
+++ src/component/menu/ReplyForm.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import { auth } from '../body/right/loginfolder/FireBase';
+import { User } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
 interface ReplyFormProps {
@@ -7,6 +9,8 @@ interface ReplyFormProps {
 
 export default function ReplyForm(props: ReplyFormProps) {
   const [value, setValue] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState<User | null>(null); // User 타입의 상태를 추가합니다.
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
@@ -15,8 +19,12 @@ export default function ReplyForm(props: ReplyFormProps) {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    props.addList(value); // addList 함수 호출
-    setValue(''); // 입력 필드 초기화
+    if (isLoggedIn) {
+      props.addList(value); // addList 함수 호출
+      setValue(''); // 입력 필드 초기화
+    } else {
+      window.alert('댓글을 등록하려면 로그인이 필요합니다.');
+    }
   };
 
   // const handleSubmit = (e: React.FormEvent) => {
@@ -24,6 +32,20 @@ export default function ReplyForm(props: ReplyFormProps) {
   //   // value 상태를 사용하여 댓글을 등록하는 로직을 구현할 수 있습니다.
   //   console.log('댓글 등록:', value);
   // };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: User | null) => {
+      // User 타입을 명시적으로 지정합니다.
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObj(user); // 사용자 정보를 저장합니다.
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <CommentForm>
