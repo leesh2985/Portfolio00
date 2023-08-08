c9dfce2850d382948445f6787479cc46e8662e1e
Seung Lee <leesh2985@naver.com>
2023-08-08 16:43:49 +0900

    게시물 링크연결

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import MyPage from './component/MyPage';
 import MainLayout from './MainLayout';
 import Contest from './component/menu/Contest';
 import Writing from './component/menu/Writing';
+import PostDetail from './component/menu/PostDetail';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/contest" element={<Contest />} />
           <Route path="home/my-page" element={<MyPage />} />
           <Route path="/writing" element={<Writing />} />
+          <Route path="/postdetail" element={<PostDetail />} />
         </Route>
         {/* Header,Footer을 안 보여주고 싶은 컴포넌트 */}
         <Route path="/login-page" element={<LoginPage />} />
