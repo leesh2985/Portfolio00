2455789c3e12b7d18e2230a55d46b45b9e07907c
Seung Lee <leesh2985@naver.com>
2023-08-11 03:21:11 +0900

    창이 변했다

--- src/component/body/right/loginfolder/Login.tsx
+++ src/component/body/right/loginfolder/Login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { auth } from './FireBase';
-import { GoogleAuthProvider, User, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, User, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import styled, { StyleSheetManager } from 'styled-components';
 import MyLogin from './MyLogin';
@@ -40,12 +40,71 @@ export default function Login() {
     }
   }, []);
 
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setValues(prev => ({ ...prev, [name]: value }));
+  };
+
+  const [errorMsg, setErrorMsg] = useState('');
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+  const handleSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!values.email || !values.password) {
+      setErrorMsg('모든 항목을 입력해주세요.');
+      return;
+    }
+    setErrorMsg('');
+
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(async res => {
+        setSubmitButtonDisabled(false);
+
+        console.log('가입 성공:', res.user);
+      })
+
+      .catch(err => {
+        setSubmitButtonDisabled(false);
+        setErrorMsg('오류가 발생했습니다: ' + err.message);
+      });
+  };
+
+  const handleRegularLogin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, values.email, values.password);
+      setValue(userCredential.user);
+      localStorage.setItem('email', JSON.stringify(userCredential.user));
+      console.log('일반 로그인 성공:', userCredential.user);
+    } catch (err) {
+      setErrorMsg('오류가 발생했습니다: ' + err.message);
+    }
+  };
+
   return (
     <StyleSheetManager shouldForwardProp={prop => prop !== 'darkMode'}>
       <LoginSection darkMode={isDarkMode}>
         {!value ? (
           <>
-            <LoginLink to="/login-page">로그인</LoginLink>
+            <LoginArea>
+              <LoginForm onSubmit={handleSubmission}>
+                <InputDiv>
+                  <LoginInput type="email" name="email" placeholder="아이디" onChange={handleChange} />
+                  <LoginInput type="password" name="password" placeholder="비밀번호" onChange={handleChange} />{' '}
+                </InputDiv>
+                <LoginButtonArea>
+                  <LoginButton type="button" onClick={handleRegularLogin} disabled={submitButtonDisabled}>
+                    로그인
+                  </LoginButton>
+                </LoginButtonArea>
+                <Error>{errorMsg}</Error>
+              </LoginForm>
+            </LoginArea>
             <SocialLink onClick={handleClick}>Google로 시작하기</SocialLink>
           </>
         ) : (
@@ -72,34 +131,37 @@ const LoginSection = styled.section<LoginSectionProps>`
   line-height: 20px;
   text-align: center;
   background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-const LoginLink = styled(Link)`
-  cursor: pointer;
-  margin: 0 auto;
-  width: 80%;
-  margin-top: 13px;
-  padding: 17px 0;
-  border: 1px solid;
-  display: block;
-  color: #fff;
-  font-size: 18px;
-  font-weight: bold;
-  border-radius: 4px;
-  background-color: #41b6e6;
-  text-decoration: none;
+// const LoginLink = styled(Link)`
+//   cursor: pointer;
+//   margin: 0 auto;
+//   width: 80%;
+//   margin-top: 13px;
+//   padding: 17px 0;
+//   border: 1px solid;
+//   display: block;
+//   color: #fff;
+//   font-size: 18px;
+//   font-weight: bold;
+//   border-radius: 4px;
+//   background-color: #41b6e6;
+//   text-decoration: none;
 
-  &:hover {
-    text-decoration: none;
-  }
-`;
+//   &:hover {
+//     text-decoration: none;
+//   }
+// `;
 
 const SocialLink = styled.button`
   cursor: pointer;
   margin: 0 auto;
   width: 80%;
   margin-top: 13px;
-  padding: 17px 0;
+  padding: 10px 0;
   border: 1px solid;
   display: block;
   color: #fff;
@@ -111,7 +173,6 @@ const SocialLink = styled.button`
 const LoginInfo = styled.div`
   margin-top: 16px;
   font-size: 1.3rem;
-  margin-bottom: 13px;
   text-align: center;
 `;
 
@@ -138,7 +199,7 @@ const JoinLink = styled(Link)`
   cursor: pointer;
   font-size: 13px;
   color: #242424;
-  text-decoration: none;
+  font-weight: 700;
 
   &::before {
     content: '';
@@ -147,5 +208,75 @@ const JoinLink = styled(Link)`
     height: 12px;
     background-color: #a9a9a9;
     margin: 4px 12px 0;
+  }
+`;
+
+/** 로그인 부분*/
+const LoginArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const LoginForm = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const InputDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const LoginInput = styled.input`
+  background-color: #fff;
+  padding: 5px 15px;
+  font-size: 18px;
+  border-radius: 4px;
+  border: 1px solid #d3d3d3;
+  outline: none;
+  width: 80%;
+  height: 20px;
+
+  &:focus {
+    border-color: #808080;
+  }
+`;
+
+const LoginButtonArea = styled.div`
+  /* display: flex;
+  align-items: center; */
+`;
+
+const Error = styled.b`
+  font-size: 12px;
+  color: red;
+  text-align: left;
+`;
+
+const LoginButton = styled.button`
+  cursor: pointer;
+  outline: none;
+  background-color: #1e8ec7;
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+  padding: 20px 15px;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid #1e8ec7;
+  transition: 100ms;
+
+  &:hover {
+    background-color: #41b6e6;
+    border: 1px solid #41b6e6;
+  }
+  &:disabled {
+    background-color: gray !important;
+    border: 1px solid gray !important;
   }
 `;
