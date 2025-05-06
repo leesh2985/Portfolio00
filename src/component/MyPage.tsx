e17de1c8f7c82d6daf892966a981f021198af042
Seung Lee <leesh2985@naver.com>
2025-05-07 00:13:38 +0900

    차크라 ui 적용안됨;;

--- src/component/MyPage.tsx
+++ src/component/MyPage.tsx
@@ -1,14 +1,17 @@
 import { auth } from './body/right/loginfolder/FireBase';
 import styled from 'styled-components';
-// import ImageUpload from './ImageUpload';
+import { Button, Flex, Heading, Text, Card, CardBody, Image } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { User, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
-export default function MyPage() {
+interface MyPageProps {
+  user: User | null;
+}
+
+export default function MyPage({ user: _ }: MyPageProps) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState<User | null>(null); // User 타입의 상태를 추가
-  // const [value, setValue] = useState<User | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,7 +37,7 @@ export default function MyPage() {
       } else {
         setIsLoggedIn(false);
         // 로그인되지 않았을 때 처리
-        alert('로그인이 필요합니다.');
+        alert('로그아웃 됐습니다.');
         navigate('/');
       }
     });
@@ -64,30 +67,63 @@ export default function MyPage() {
     <MyContainer>
       {isLoggedIn ? (
         // 사용자가 로그인한 경우
-        <div>
-          <ProFile>
-            {/* <ImageUpload /> */}
-            <TextArea>
-              <DisplayName>{userObj.displayName}님</DisplayName>
-              <RunningRecord>평균기록 07.35</RunningRecord>
-            </TextArea>
-          </ProFile>
-          <Marathon>
-            <MarathonText>마라톤 참여</MarathonText>
-            <ImgBox>
-              <MarathonImg src="/post/post1.jpg" alt="기록"></MarathonImg>
-              <MarathonImg src="/post/post2.jpg" alt="기록"></MarathonImg>
-            </ImgBox>
-            <SubBtn>개인정보수정</SubBtn>
-            <LogoutBtn onClick={handleLogout}>로그아웃</LogoutBtn>
-          </Marathon>
-        </div>
+        <>
+          <Flex direction="column" align="center" gap={6} py={10}>
+            {/* 프로필 이미지 원형 카드 */}
+            <Card borderRadius="full" overflow="hidden" boxShadow="sm" w="150px" h="150px" border="1px solid #d3d3d3">
+              <CardBody p={0}>
+                <Image src="/img/profile.png" alt="프로필" objectFit="cover" width="100%" height="100%" />
+              </CardBody>
+            </Card>
+
+            {/* 이름 및 기록 텍스트 영역 */}
+            <Flex direction="column" align="center" gap={2}>
+              <Text fontSize="xl" fontWeight="bold">
+                {userObj.displayName}님
+              </Text>
+              <Text fontSize="md" color="gray.600">
+                평균기록 06.35
+              </Text>
+            </Flex>
+          </Flex>
+          <Flex direction="column" align="center" mt={8}>
+            {/* 마라톤 제목 */}
+            <Heading as="h2" size="2xl" mb={4}>
+              참여한 마라톤
+            </Heading>
+
+            {/* 이미지 목록 */}
+            <Flex gap={4} flexWrap="wrap" mb={6}>
+              <Image src="/post/post1.jpg" alt="기록1" boxSize="200px" objectFit="cover" borderRadius="md" />
+              <Image src="/post/post2.jpg" alt="기록2" boxSize="200px" objectFit="cover" borderRadius="md" />
+              <Image src="/img/대회1.png" alt="기록2" boxSize="200px" objectFit="cover" borderRadius="md" />
+              <Image src="/img/대회7.png" alt="기록2" boxSize="200px" objectFit="cover" borderRadius="md" />
+            </Flex>
+          </Flex>
+          <Flex align="center" justify="center" gap={4} mt={6}>
+            <Button colorScheme="blue" size="md" fontWeight="600" borderRadius="full" px={6} py={2} cursor="pointer">
+              개인정보수정
+            </Button>
+
+            <Button
+              variant="outline"
+              colorScheme="gray"
+              size="md"
+              fontWeight="600"
+              px={6}
+              py={2}
+              onClick={handleLogout}
+              cursor="pointer">
+              로그아웃
+            </Button>
+          </Flex>
+        </>
       ) : (
         // 사용자가 로그인하지 않은 경우
-        <div>
+        <>
           {/* 홈 화면으로 이동 또는 다른 처리를 할 수 있습니다. */}
           로그인이 필요합니다.
-        </div>
+        </>
       )}
     </MyContainer>
   );
@@ -95,80 +131,7 @@ export default function MyPage() {
 
 const MyContainer = styled.div`
   margin: 0 auto;
-  max-width: 1380px;
-  padding-top: 50px;
+  max-width: 1280px;
+  padding-top: 30px;
   padding-bottom: 50px;
-`;
-
-const ProFile = styled.div`
-  display: flex;
-  margin: 0 auto;
-  max-width: 1380px;
-  padding-top: 50px;
-  padding-bottom: 50px;
-  align-items: center;
-`;
-
-const TextArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 30px;
-  align-items: flex-start;
-  /* margin-left: 50px; */
-`;
-
-const DisplayName = styled.span`
-  font-size: 25px;
-  font-weight: bold;
-  justify-self: start;
-`;
-
-const RunningRecord = styled.span`
-  font-size: 20px;
-  justify-self: start;
-`;
-
-const Marathon = styled.div``;
-
-const MarathonText = styled.p`
-  text-align: left;
-  font-weight: bold;
-  font-size: 25px;
-`;
-
-const ImgBox = styled.div`
-  display: flex;
-  margin-top: 10px;
-`;
-
-const MarathonImg = styled.img`
-  width: 200px;
-  height: auto;
-  margin-right: 10px;
-  &:last-child {
-    margin-right: 0px;
-  }
-`;
-const SubBtn = styled.button`
-  font-weight: 600;
-  font-size: 18px;
-  padding: 5px 20px;
-  cursor: pointer;
-  color: #fff;
-  border-radius: 15px;
-  border: 1px solid #41b6e6;
-  background-color: #41b6e6;
-  margin-top: 30px;
-`;
-const LogoutBtn = styled.button`
-  font-weight: 600;
-  font-size: 18px;
-  padding: 5px 20px;
-  cursor: pointer;
-  color: #fff;
-  border-radius: 15px;
-  border: 1px solid #d3d3d3;
-  background-color: #d3d3d3;
-  margin-top: 30px;
-  margin-left: 50px;
 `;
